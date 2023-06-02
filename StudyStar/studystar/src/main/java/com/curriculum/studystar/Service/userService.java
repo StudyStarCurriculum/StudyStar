7f13134b63ff3bc0b51a74d42f0a5bbc7f789bfa
MaverickQu <74614702+JaceBelern@users.noreply.github.com>
2023-06-02 09:51:19 +0800

    Delete userService.java

--- StudyStar/studystar/src/main/java/com/curriculum/studystar/Service/userService.java
+++
@@ -1,8 +0,0 @@
-package com.curriculum.studystar.Service;
-
-import com.curriculum.studystar.Domain.RequestAndResponse.Request.UserRequest.LoginRequest;
-import com.curriculum.studystar.Domain.RequestAndResponse.Response.UserResponse.LoginResponse;
-
-public interface UserService {
-    public LoginResponse Login(LoginRequest req);
-}
